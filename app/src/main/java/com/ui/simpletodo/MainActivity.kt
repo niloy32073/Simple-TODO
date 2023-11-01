05bad4ec20923b246969c70951b10560af6c6dcb
niloy32073 <mdnifaurrahmanx27@gmail.com>
2023-11-02 02:11:58 +0600

    Basic UI Completed

--- app/src/main/java/com/ui/simpletodo/MainActivity.kt
+++ app/src/main/java/com/ui/simpletodo/MainActivity.kt
@@ -12,10 +12,18 @@ import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -35,15 +43,30 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    val sheetState = rememberModalBottomSheetState()
+                    val scope = rememberCoroutineScope()
+                    var isOpen by remember { mutableStateOf(false) }
+
                     Scaffold (
-                        content ={MainScreen()},
                         floatingActionButtonPosition = FabPosition.End,
                         floatingActionButton = {
-                            FloatingActionButton(onClick = { /*TODO*/ }, containerColor = Color.Blue, content = {
+                            FloatingActionButton(
+                                onClick = {
+                                    isOpen = true
+                                },
+                                containerColor = Color.Blue,
+                                content = {
                                 Icon(imageVector = Icons.Filled.Add, contentDescription = "Add")
                             })
                         }
-                    )
+                    ){
+                        MainScreen()
+                        if (isOpen){
+                            ModalBottomSheet(sheetState =sheetState ,onDismissRequest = { isOpen = false }) {
+                                Text(text = "Hello")
+                            }
+                        }
+                    }
                    
                 }
             }
