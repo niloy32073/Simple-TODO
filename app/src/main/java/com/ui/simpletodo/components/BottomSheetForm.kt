a11e6317ecb06f5e16a9d473d177bfa535736ab1
niloy32073 <mdnifaurrahmanx27@gmail.com>
2023-11-02 19:02:25 +0600

    Bottom Sheet Updated

--- app/src/main/java/com/ui/simpletodo/components/BottomSheetForm.kt
+++ app/src/main/java/com/ui/simpletodo/components/BottomSheetForm.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.OutlinedTextField
@@ -21,20 +22,16 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun BottomSheetForm(){
     var title by remember { mutableStateOf("") }
+    var details by remember { mutableStateOf("") }
     var isExpanded by remember { mutableStateOf(false) }
     var category by remember { mutableStateOf(0) }
     val categoryNames = listOf<String>("High","Mid","Low")
     var selectedCategory by remember { mutableStateOf("Priority") }
-    Column(modifier = Modifier.height(200.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+    Column(modifier = Modifier.height(400.dp), horizontalAlignment = Alignment.CenterHorizontally) {
         OutlinedTextField(modifier = Modifier.fillMaxWidth(),value = title , onValueChange ={ title = it} , label = { Text(text = "Task Title" )})
-        Text(text = selectedCategory , modifier = Modifier.clickable { isExpanded = true })
-        DropdownMenu(expanded = isExpanded, onDismissRequest = { isExpanded = false }) {
-            categoryNames.forEachIndexed{
-                index,title->
-                DropdownMenuItem(text = { Text(text = title) }, onClick = { category = index
-                    selectedCategory =  title
-                isExpanded = false})
-            }
+        OutlinedTextField(modifier = Modifier.fillMaxWidth(),value = title , onValueChange ={ title = it} , label = { Text(text = "Details" )})
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Add")
         }
     }
 
